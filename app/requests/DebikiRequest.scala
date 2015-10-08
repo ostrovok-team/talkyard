09c8e7e81ec586095a79c2a293a5e4728d3787e4
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-10-09 01:58:18 +0200

    Anti spam. For now: Akismet, Spamhaus, uribl and StopForumSpam
    Later: All things Discourse does :-) See comment in class AntiSpam.

--- app/requests/DebikiRequest.scala
+++ app/requests/DebikiRequest.scala
@@ -58,8 +58,9 @@ abstract class DebikiRequest[A] {
   def anyRoleId = user.flatMap(_.anyRoleId)
   def theRoleId = anyRoleId getOrElse throwForbidden("DwE86Wb7", "Not authenticated")
 
-  def isAuthenticated = user.map(_.isAuthenticated) == Some(true)
-  def isApprovedOrStaff = user.map(_.isApprovedOrStaff) == Some(true)
+  def isAuthenticated = user.exists(_.isAuthenticated)
+  def isApprovedOrStaff = user.exists(_.isApprovedOrStaff)
+  def isStaff = user.exists(_.isStaff)
 
   /**
    * The display name of the user making the request. Throws 403 Forbidden
