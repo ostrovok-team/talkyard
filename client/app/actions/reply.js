7bcffd30474269647fda41c1954b2cba95649044
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2017-08-02 20:45:22 +0200

    Make embedded comments work again, wip.

--- client/app/actions/reply.js
+++ client/app/actions/reply.js
@@ -19,15 +19,9 @@
 
 var d = { i: debiki.internal };
 
-/*
-function sendWriteReplyMessageToEmbeddedEditor(postId) {
-  window.parent.postMessage(
-      JSON.stringify(['editorToggleReply', postId]), '*');
-}; */
-
 
 d.i.clearIsReplyingMarks = function() {
-  const replyBtns = $$byClass('dw-replying');
+  const replyBtns = debiki2.$$byClass('dw-replying');
   for (var i = 0; i < replyBtns.length; ++i) {
     const buttonElem = replyBtns[i];
     buttonElem.classList.remove('dw-replying');
